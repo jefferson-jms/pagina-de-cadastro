d65757ec498448d7415df0bd0d8b121928a100c2
jefferson-jms <83091992+jefferson-jms@users.noreply.github.com>
2021-09-25 02:15:22 -0300

    editado scrpit js para adicionar os campos de endereço automático após inserir o cep

---
+++ js/script.js
@@ -0,0 +1,17 @@
+		$("#cep").focusout(function(){
+			$.ajax({
+				url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
+				//Aqui você deve preencher o tipo de dados que será lido,
+				dataType: 'json',
+				//SUCESS é referente a função que será executada caso
+				//ele consiga ler a fonte de dados com sucesso.
+				success: function(resposta){
+					//Agora basta definir os valores que você deseja preencher
+					$("#rua").val(resposta.logradouro);
+					$("#complemento").val(resposta.complemento);
+					$("#bairro").val(resposta.bairro);
+					$("#cidade").val(resposta.localidade);
+					$("#estado").val(resposta.uf);
+				}
+			});
+		});
